560ce0f34365e589bd33c3f76a39f96554c2de04
siddhardha varma <sid@pop-os.localdomain>
2022-11-27 00:14:46 +0530

    created graphql query

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const {graphqlHTTP} = require(
+    'express-graphql'
+)
+const connectDB = require('./config/db.js')
+const schema = require('./schema/schema.js')
+const port = process.env.PORT || 5000;
+const app = express();
+//db connection
+connectDB();
+//graphql middleware
+app.use(
+    '/graphql',graphqlHTTP({
+        schema,
+        graphiql : process.env.NODE_ENV == "dev",
+    })
+)
+app.listen(port,()=>{
+    console.log(`app started on port ${port}`)
+})
