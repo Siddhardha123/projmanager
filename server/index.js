6b5c6fbe01fdd54c8f691cc04306d70fce56e651
siddhardha varma <sid@pop-os.localdomain>
2022-11-27 18:43:25 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const {graphqlHTTP} = require(
     'express-graphql'
 )
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db.js')
 const schema = require('./schema/schema.js')
@@ -18,6 +19,12 @@ app.use(
         graphiql : process.env.NODE_ENV == "dev",
     })
 )
+app.use(express.static('public'));
+app.get(
+    '*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    }
+);
 app.listen(port,()=>{
     console.log(`app started on port ${port}`)
 })
